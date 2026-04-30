501f4e4d48e4e1983c8b2b6dda95787769811dcb
Rishith <rishithummenthala@gmail.com>
2026-04-30 12:07:00 +0530

    Initial project setup

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+module.exports = app;
